5c3830fc6b9dae3e251132beec0b4e88cd5b4bd7
Rachel <rachel17.22@gmail.com>
2021-03-10 14:12:36 -0700

    task component created

---
+++ src/assets/components/Task.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Task({
+                task: {id, title, state},
+                onArchiveTask,
+                onPinTask,
+              }) {
+  return (
+    <div className="list-item">
+      <input type="text" value={title} readOnly={true} />
+    </div>
+  );
+}
+
+export default Task;
